0ca7e4a69f06236f33708fbc48b854e1cb22484b
Mr-zuo <841797097@qq.com>
2021-05-29 17:08:48 +0800

    update

--- src/main/java/com/ron/easydoc/EasydocApplication.java
+++ src/main/java/com/ron/easydoc/EasydocApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EasydocApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(EasydocApplication.class, args);
     }
